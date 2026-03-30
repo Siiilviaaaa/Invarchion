808acb96a7bec401e8b915f808e485b5a2f09c2b
Darcov18 <167454430+Darcov18@users.noreply.github.com>
2026-03-30 09:57:37 +0200

    prueba

--- Archon1/Archon1/Main.cpp
+++ Archon1/Archon1/Main.cpp
@@ -1,3 +1,5 @@
 //soy Diego, he conseguido sincronizar el GitHub con mi VisualStudio y soy capaz de acceder
 //los permisos estan bien concedidos, procedo a hacer pruebas de push  y pull
-//termino las pruebas de configuración del proyecto
+//termino las pruebas de configuración del proyecto e
+
+//practica nueva 
