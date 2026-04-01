5b9592269019bea9d1236686c86c06eef26e662e
JULIANA RODRIGUEZ FILIPPO <juliana.rodriguez@alumnos.upm.es>
2026-04-01 13:25:41 +0200

    algunas funciones en juego.cpp

--- Archon1/src/Juego.cpp
+++ Archon1/src/Juego.cpp
@@ -2,17 +2,26 @@
 
 void Juego::IniciarJuego() {
 
-	int turno{ TurnoHumanos };
-	bool ejecutandose{ 0 };
+    ejecutandose = 1;
 
-    // Constructor cuando sepamos como va le añadiré cosas
-    Juego();
+    //// Constructor cuando sepamos como va le añadiré cosas
+    //Juego();
 
-    // Destructor lo mismo
-    ~Juego();
+    //// Destructor lo mismo
+    //~Juego();
 
-    
+    //hay que decri que se cree el menu, el teblero y la pantalla de batalla
 };
+
+void Juego::cambiarTurno() {
+    if (turnoActual == TurnoHumanos) turnoActual = TurnoAliens;
+    else turnoActual = TurnoHumanos;
+}
+
+int Juego::getTurno() {
+    return turnoActual;
+}
+
 void Juego::procesarEntradas() {
 
 }
@@ -30,6 +39,10 @@ void Juego::dibujarCapoBatalla() {
 
 }
 
-void Juego::dibujarMenu();
+void Juego::dibujarMenu() {
 
-void ejecutar()
+}
+
+void Juego::ejecutar() {
+
+}
--- Archon1/src/Main.cpp
+++ Archon1/src/Main.cpp
@@ -2,12 +2,17 @@
 #include "freeglut.h"
 #include "ETSIDI.h"
 #include "tablero.h"
+#include "Juego.h"
 
 //porfavor no toqueis rutas de carpetas q me ha costado mucho y lloro, el archivo gitgnore igual
 //llevo un par de horas para guardar temas de la carpeta de extra, confirmadme si se escucha
 //hoy no se si me da tiempo a terminar de configurar el main, de ahi en adelante con todo
 //si quereis configurarlo vosotras recordad q es copiar las funciones de freeglut del lab
 //como se rompa lloro
+
+Juego Invarchion;
+
+
 Tablero miTablero;
 void OnDraw(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -19,6 +24,10 @@ void OnDraw(void) {
     glutSwapBuffers();
 }
 int main(int argc, char** argv) {
+    //INICIAR JUEGO
+    Invarchion.IniciarJuego(); //esto cambia el valor del bool ejecutandose a 1, por lo que podeis poner las funciones como la de dibujar el tablero en basse a esto
+
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(800, 600);
