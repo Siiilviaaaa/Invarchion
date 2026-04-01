63cfcf8fa0a6d598ebe802d26345547758d8c635
JULIANA RODRIGUEZ FILIPPO <juliana.rodriguez@alumnos.upm.es>
2026-04-01 12:40:43 +0200

    funicones en juego.cpp pero me he equivocado y lo estaba haciendo en el main

--- Archon1/src/Juego.cpp
+++ Archon1/src/Juego.cpp
@@ -1,16 +1,35 @@
-//#include "Juego.h"
-//
-//void IniciarJuego() {
-//private:
-//	int turno{ TurnoHumanos };
-//	bool ejecutandose{ 0 };
-//
-//public:
-//    // Constructor cuando sepamos como va le añadiré cosas
-//    Juego();
-//
-//    // Destructor lo mismo
-//    ~Juego();
-//
-//    
-//};
+#include "Juego.h"
+
+void Juego::IniciarJuego() {
+
+	int turno{ TurnoHumanos };
+	bool ejecutandose{ 0 };
+
+    // Constructor cuando sepamos como va le añadiré cosas
+    Juego();
+
+    // Destructor lo mismo
+    ~Juego();
+
+    
+};
+void Juego::procesarEntradas() {
+
+}
+
+void Juego::actualizar() {
+
+}
+
+
+void Juego::dibujarTablero() {
+
+}
+
+void Juego::dibujarCapoBatalla() {
+
+}
+
+void Juego::dibujarMenu();
+
+void ejecutar()
--- Archon1/src/Juego.h
+++ Archon1/src/Juego.h
@@ -1,34 +1,34 @@
-//#pragma once
-//class Juego
-//{
-//	enum EstadosJuego :int {
-//		EstadoMenu,
-//		EstadoTablero,
-//		EstadoBatalla,
-//		EstadoFinDePartida
-//	};
-//
-//	enum Turno : int {
-//		TurnoHumanos,
-//		TurnoAliens
-//	};
-//
-//	void IniciarJuego();
-//
-//	
-//	void procesarEntradas(); //teclas y tal
-//
-//	void actualizar();
-//
-//	
-//	void dibujarTablero();
-//
-//	void dibujarCapoBatalla();
-//
-//	void dibujarMenu();
-//
-//	void ejecutar();
-//
-//
-//};
+#pragma once
+class Juego
+{
+	enum EstadosJuego :int {
+		EstadoMenu,
+		EstadoTablero,
+		EstadoBatalla,
+		EstadoFinDePartida
+	};
+
+	enum Turno : int {
+		TurnoHumanos,
+		TurnoAliens
+	};
+
+	void IniciarJuego();
+
+	
+	void procesarEntradas(); //teclas y tal
+
+	void actualizar();
+
+	
+	void dibujarTablero();
+
+	void dibujarCapoBatalla();
+
+	void dibujarMenu();
+
+	void ejecutar();
+
+
+};
 
