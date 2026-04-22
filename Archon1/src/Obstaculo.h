b1fe86147c5f5f39b70e1ff2c181f5aad05bad65
Silvia Mera <silvia.mera@alumnos.upm.es>
2026-04-22 10:54:12 +0200

    Cabeceras de funciones para rebotes

--- Archon1/src/Obstaculo.h
+++ Archon1/src/Obstaculo.h
@@ -1,12 +1,18 @@
 #pragma once
+#include "Disparos.h"
+#include "Personajes.h"
 
 class Obstaculo
 {
 	double x, y;
 	double radio;
+	//SERA ALGO GRAFICO O UNA IMAGEN??
+
 public:
 	Obstaculo(double pos_x, double pos_y, double r);
-	bool colisiona(double px, double py, double pr) const;
+
+	static void reboteDisparo(Disparo& d);
+	static void NoPasar(Personaje& j1, Personaje& j2);
 
 	double getX() const { return x; }
 	double getY() const { return y; }
