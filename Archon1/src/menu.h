933375c75d31fc64263c77b86601c53644e5f83a
Darcov18 <167454430+Darcov18@users.noreply.github.com>
2026-05-29 20:42:52 +0200

    comebtaruis del main organizados para su correcta comprension continuo con otros archivos

--- Archon1/src/menu.h
+++ Archon1/src/menu.h
@@ -19,18 +19,14 @@ private:
     ETSIDI::Sprite _ranking;
 
     std::vector<std::string> _lineas_ranking;
-
-    // CORRECCIÓN: El vector debe ser un miembro de la clase
     std::vector<EntradaRanking> _lista_puntuaciones;
 
 public:
     Menu();
 
     void inicializa_menu();
-    // Cambia la línea actual por esta en la sección pública de tu clase Menu:
     void dibuja_menu(bool hJugar = false, bool hRanking = false, bool hSalir = false);
     void dibuja_capa_seleccion();
-
     void dibuja_ranking();
     void cargar_ranking();
     void actualizar_ranking(std::string nombre);
